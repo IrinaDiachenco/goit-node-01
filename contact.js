8b69e5615900e795ef639596671d9e64bdc2ee8f
IrinaDiachenco <65997430+IrinaDiachenco@users.noreply.github.com>
2021-04-10 11:37:22 +0300

    Update contact.js

--- contact.js
+++ contact.js
@@ -6,26 +6,23 @@ const contactsPath = path.join(__dirname, 'db', 'contacts.json')
 // TODO: задокументировать каждую функцию
 function listContacts() {
   // ...твой код
-fs.readdir(__dirname)
-  .then((contacts) => {
-    return Promise.all(
-      contacts.map(async (id) => {
-        const stats = await fs.stat(id)
-        return {
-          Id: id,
-          Name: stats.name,
-          Email: stats.email,
-          Phone: stats.phone,
-        }
-      }),
-    )
-  })
-  .then((result) => console.table(result))
+fs.readFile(contactsPath)
+  .then((data) => console.log(data.toString()))
+  .catch((err) => console.log(err.message))
 }
 
 function getContactById(contactId) {
   // ...твой код
+fs.readFile(contactsPath)
+    .then((data) => {
+        const users = JSON.parse(data)
+        const user = users.find(user => user.id === contactId)
+        console.log(user)
+    })
+  .catch((err) => console.log(err.message))
 }
+
+getContactById()
 
 function removeContact(contactId) {
   // ...твой код
